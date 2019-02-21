41be816d71cb82cb53fe8be82f12404e8bd29e27
Charles Campbell <charles.e.campbell1989@gmail.com>
2019-02-21 08:51:02 -0500

    inital commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+// getRandomQuote() -- gets a random number and returns it. The random number will be the index in the Quote Array of the  random quote to use.
+function getRandomQuote() {
+  var quoteIndex = Math.floor(Math.random() * (quotes.length));
+  return quoteIndex;
+}
+
+// getRandomColor() -- generates a random number to be used in making a random RGB color.
+function getRandomColor() {
+  var randomNumber = Math.floor(Math.random() * 255);
+  return randomNumber;
+}
+
+// setBackgroundColor() -- sets the background color of the body element.
+function setBackgroundColor() {
+  let h = getRandomColor();
+  let s = getRandomColor();
+  let hsl = "hsl(" + h + ", " + s + "%, 20%, 0.92)"
+  let backgroundColor = document.getElementById('body');
+  backgroundColor.style.cssText = "background: linear-gradient(" + hsl + ", " + hsl + "), url(img/lotr.jpg)";
+}
+
+// printQuote() -- calls the getRandomQuote function and prints the random quote chosen to the screen.
+function printQuote() {
+  var body = document.getElementById("quote_box");
+  var quoteSource = document.getElementById("source");
+  let randomNum = getRandomQuote();
+  quote_box.innerHTML = quotes[randomNum].quote;
+  source.innerHTML = quotes[randomNum].source;
+}
+
+let buttonClick = document.getElementById("button");
+buttonClick.onclick = function () {
+  setBackgroundColor();
+  printQuote();
+}
+
+setBackgroundColor();
+printQuote();
---
+++ js/data.js
@@ -0,0 +1,45 @@
+// this is the array to be used for random quotes (need atleast 5)
+let quotes = [{
+    quote: "Nobody tosses a Dwarf!",
+    source: "- Gimli",
+    
+  },
+  {
+    quote: "One does not simply walk into Mordor",
+    source: "- Boromir"
+  },
+  {
+    quote: "You shall not pass!",
+    source: "- Gandalf",
+    
+  },
+  {
+    quote: "You need people of intelligence on this sort of mission...quest...thing.",
+    source: "- Pippin"
+  },
+  {
+    quote: "A wizard is never late, Frodo Baggins. Nor is he early. He arrives precisely when he means to.",
+    source: "- Gandalf",
+    
+  },
+  {
+    quote: "Memory is not what the heart desires. That is only a mirror.",
+    source: "- J.R.R Tolkien"
+  },
+  {
+    quote: "All we have to decide is what to do with the time that is given us.",
+    source: "- Gandalf"
+  },
+  {
+    quote: "I’m glad to be with you, Samwise Gamgee…here at the end of all things.",
+    source: "- Frodo"
+  },
+  {
+    quote: "Deeds will not be less valiant because they are unpraised.",
+    source: "- Aragorn"
+  },
+  {
+    quote: "Don’t adventures ever have an end? I suppose not. Someone else always has to carry on the story.",
+    source: "- Bilbo"
+  },
+];
