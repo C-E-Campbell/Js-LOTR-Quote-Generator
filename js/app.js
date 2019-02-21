f049a5c888605e0d4151d00b80a1758db9be7dfe
Charles Campbell <charles.e.campbell1989@gmail.com>
2019-02-21 12:10:39 -0500

    color changes

--- js/app.js
+++ js/app.js
@@ -18,9 +18,9 @@ function setBackgroundColor() {
   let hsl1 = "hsl(" + hue + ", 45%, 20%, 0.85)";
   let hsl2 = "hsl(" + hue2 + ", 45%, 40%, 0.85)";
   let backgroundColor = document.getElementById('body');
-  backgroundColor.style.cssText = "background: linear-gradient(" + hsl1 + ", " + hsl2 + "), url(https://s.aolcdn.com/hss/storage/midas/7d48de207b934a3e3a63e2aab34d2d09/205858706/lord_of_the_rings.jpg)";
+  backgroundColor.style.cssText = "background: linear-gradient(155deg , " + hsl1 + ", " + hsl2 + "), url(https://s.aolcdn.com/hss/storage/midas/7d48de207b934a3e3a63e2aab34d2d09/205858706/lord_of_the_rings.jpg)";
 }
-}
+
 
 // printQuote() -- calls the getRandomQuote function and prints the random quote chosen to the screen.
 function printQuote() {
