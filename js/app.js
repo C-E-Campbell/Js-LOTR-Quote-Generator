391725af9ec5f13ed9e9f79ab9a738b72aaa2517
Charles Campbell <charles.e.campbell1989@gmail.com>
2019-02-23 11:37:10 -0500

    pictures added

--- js/app.js
+++ js/app.js
@@ -1,18 +1,12 @@
 // getRandomQuote() -- gets a random number and returns it. The random number will be the index in the Quote Array of the  random quote to use.
-function getRandomQuote() {
-  var quoteIndex = Math.floor(Math.random() * (quotes.length));
-  return quoteIndex;
-}
-
+const getRandomQuote = () => Math.floor(Math.random() * (quotes.length));
+  
 // getRandomColor() -- generates a random number to be used in making a random HSL color.
-function getRandomColor() {
-  var randomNumber = Math.floor(Math.random() * 360);
-  return randomNumber;
-}
+const getRandomColor = () => Math.floor(Math.random() * 360);
 
 // setBackgroundColor() -- sets the background color of the body element.
 // side note -- hsl was used so I could make sure the colors wouldnt be too bright against the white font text. lightness is set to 20% always.
-function setBackgroundColor() {
+const  setBackgroundColor = () => {
   let hue = getRandomColor();
   let hue2 = getRandomColor();
   let hsl1 = "hsl(" + hue + ", 45%, 20%, 0.85)";
@@ -23,13 +17,15 @@ function setBackgroundColor() {
 
 
 // printQuote() -- calls the getRandomQuote function and prints the random quote chosen to the screen.
-function printQuote() {
+const printQuote = () => {
   let body = document.getElementById("quote_box");
   let quoteSource = document.getElementById("source");
   let quoteMovie = document.getElementById("movie");
+  let quotePicture = document.getElementById("picture");
   let randomNum = getRandomQuote();
   quote_box.innerHTML = "<p>" + quotes[randomNum].quote + "</p>";
   source.innerHTML = quotes[randomNum].source;
+  picture.innerHTML = "<img src = " + quotes[randomNum].picture + ">";
 
   //  this IF STATEMENT checks to see if the random quote has a movie property of something other than undefined: if it does we add it to the page.
   // Only three of the quotes have a movie property.
@@ -46,5 +42,6 @@ buttonClick.onclick = function () {
   printQuote();
 }
 
+// calling the two functions to set the intial quote and color, the user will use the button going forwards.
 setBackgroundColor();
 printQuote();
--- js/data.js
+++ js/data.js
@@ -2,62 +2,81 @@
 let quotes = [{
     quote: "Nobody tosses a Dwarf!",
     source: "- Gimli",
-
+    picture: "../img/gimli.jpeg"
   },
   {
     quote: "One does not simply walk into Mordor",
-    source: "- Boromir"
+    source: "- Boromir",
+    picture: "../img/boromir.jpg"
   },
   {
     quote: "You shall not pass!",
     source: "- Gandalf",
+    picture: "../img/gandalf.jpg"
 
   },
   {
     quote: "You need people of intelligence on this sort of mission...quest...thing.",
-    source: "- Pippin"
+    source: "- Pippin",
+    picture: "../img/pippin.jpg"
   },
   {
     quote: "A wizard is never late, Frodo Baggins. Nor is he early. He arrives precisely when he means to.",
     source: "- Gandalf",
+    picture: "../img/gandalf.jpg"
 
   },
   {
     quote: "Memory is not what the heart desires. That is only a mirror.",
-    source: "- J.R.R Tolkien"
+    source: "- J.R.R Tolkien",
+    picture: "../img/tolkien.jpg"
   },
   {
     quote: "All we have to decide is what to do with the time that is given us.",
-    source: "- Gandalf"
+    source: "- Gandalf",
+    picture: "../img/gandalf.jpg"
   },
   {
     quote: "I’m glad to be with you, Samwise Gamgee…here at the end of all things.",
-    source: "- Frodo"
+    source: "- Frodo",
+    picture: "../img/frodo.jpg"
   },
   {
     quote: "Deeds will not be less valiant because they are unpraised.",
-    source: "- Aragorn"
+    source: "- Aragorn",
+    picture: "../img/aragorn.jpg"
   },
   {
     quote: "Don’t adventures ever have an end? I suppose not. Someone else always has to carry on the story.",
-    source: "- Bilbo"
+    source: "- Bilbo",
+    picture: "../img/bilbo.png"
   },
   {
     quote: "Not all those who wander are lost.",
-    source: "- J.R.R Tolkien"
+    source: "- J.R.R Tolkien",
+    picture: "../img/tolkien.jpg"
   },
   {
     quote: "He that breaks a thing to find out what it is, has left the path of wisdom.",
-    source: "- Gandalf"
+    source: "- Gandalf",
+    picture: "../img/gandalf.jpg"
   },
   {
     quote: "We wants it, we needs it. Must have the precious. They stole it from us. Sneaky little hobbitses. Wicked, tricksy, false!",
     source: "- Gollum",
-    movie: "The Two Towers"
+    movie: "The Two Towers",
+    picture: "../img/gollum.jpg"
   },
   {
     quote: "The mightiest man may be slain by one arrow.",
     source: "- Pippin",
-    movie: "The Return of the King"
+    movie: "The Return of the King",
+    picture: "../img/pippin.jpg"
+  },
+  {
+    quote: "...And you have my bow.",
+    source: "- Legolas",
+    movie: "The Fellowship of the Ring",
+    picture: "../img/legolas.jpg"
   }
 ];
