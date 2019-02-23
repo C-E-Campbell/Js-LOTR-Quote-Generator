eee38f3687d998d72636f8266abb7a8732566265
Charles Campbell <charles.e.campbell1989@gmail.com>
2019-02-23 17:21:53 -0500

    picture changes

--- js/data.js
+++ js/data.js
@@ -2,81 +2,81 @@
 let quotes = [{
     quote: "Nobody tosses a Dwarf!",
     source: "- Gimli",
-    picture: "../img/gimli.jpeg"
+    picture: "https://www.sideshow.com/product-asset/903529/feature"
   },
   {
     quote: "One does not simply walk into Mordor",
     source: "- Boromir",
-    picture: "../img/boromir.jpg"
+    picture: "https://vignette.wikia.nocookie.net/factpile/images/6/66/Lotr-boromir-1280jpg-b6a4d5_1280w.jpg/revision/latest?cb=20160331232305"
   },
   {
     quote: "You shall not pass!",
     source: "- Gandalf",
-    picture: "../img/gandalf.jpg"
+    picture: "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2012/09/Gandalf_a_l.jpg"
 
   },
   {
     quote: "You need people of intelligence on this sort of mission...quest...thing.",
     source: "- Pippin",
-    picture: "../img/pippin.jpg"
+    picture: "https://i.pinimg.com/originals/94/28/db/9428db22d17aaad9d0f8e5baa5261445.jpg"
   },
   {
     quote: "A wizard is never late, Frodo Baggins. Nor is he early. He arrives precisely when he means to.",
     source: "- Gandalf",
-    picture: "../img/gandalf.jpg"
+    picture: "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2012/09/Gandalf_a_l.jpg"
 
   },
   {
     quote: "Memory is not what the heart desires. That is only a mirror.",
     source: "- J.R.R Tolkien",
-    picture: "../img/tolkien.jpg"
+    picture: "https://ichef.bbci.co.uk/news/660/cpsprodpb/54A6/production/_85207612_tolkienbbc976.jpg"
   },
   {
     quote: "All we have to decide is what to do with the time that is given us.",
     source: "- Gandalf",
-    picture: "../img/gandalf.jpg"
+    picture: "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2012/09/Gandalf_a_l.jpg"
   },
   {
     quote: "I’m glad to be with you, Samwise Gamgee…here at the end of all things.",
     source: "- Frodo",
-    picture: "../img/frodo.jpg"
+    picture: "http://4.bp.blogspot.com/-90hwewRVQtg/VahzGgst5PI/AAAAAAAAD1Q/ckwkglwJ3wU/s1600/frodo%2Bsmiling.jpg"
   },
   {
     quote: "Deeds will not be less valiant because they are unpraised.",
     source: "- Aragorn",
-    picture: "../img/aragorn.jpg"
+    picture: "https://i.pinimg.com/originals/2d/2d/16/2d2d1655333a51284c911178f8b03775.jpg"
   },
   {
     quote: "Don’t adventures ever have an end? I suppose not. Someone else always has to carry on the story.",
     source: "- Bilbo",
-    picture: "../img/bilbo.png"
+    picture: "https://www.universeguide.com/mobile/bilbobagginsfromlordoftherings.jpg"
   },
   {
     quote: "Not all those who wander are lost.",
     source: "- J.R.R Tolkien",
-    picture: "../img/tolkien.jpg"
+    picture: "https://ichef.bbci.co.uk/news/660/cpsprodpb/54A6/production/_85207612_tolkienbbc976.jpg"
   },
   {
     quote: "He that breaks a thing to find out what it is, has left the path of wisdom.",
     source: "- Gandalf",
-    picture: "../img/gandalf.jpg"
+    picture: "https://cdn1.thr.com/sites/default/files/imagecache/scale_crop_768_433/2012/09/Gandalf_a_l.jpg"
   },
   {
     quote: "We wants it, we needs it. Must have the precious. They stole it from us. Sneaky little hobbitses. Wicked, tricksy, false!",
     source: "- Gollum",
     movie: "The Two Towers",
-    picture: "../img/gollum.jpg"
+    picture: "https://www.irishtimes.com/polopoly_fs/1.2453158.1449152864!/image/image.jpg_gen/derivatives/box_620_330/image.jpg"
   },
   {
     quote: "The mightiest man may be slain by one arrow.",
     source: "- Pippin",
     movie: "The Return of the King",
-    picture: "../img/pippin.jpg"
+    picture: "https://i.pinimg.com/originals/94/28/db/9428db22d17aaad9d0f8e5baa5261445.jpg"
   },
   {
     quote: "...And you have my bow.",
     source: "- Legolas",
     movie: "The Fellowship of the Ring",
-    picture: "../img/legolas.jpg"
+    picture: "https://static.comicvine.com/uploads/original/11111/111114474/3858116-legolas.jpg"
   }
 ];
